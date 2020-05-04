f663e9c999f930ce52dc879924d5c50c3dae8110
Nattawut Visuttipinat <nattawut@magicboxasia.com>
2020-05-05 00:35:44 +0700

    deploy

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
     secret : 'adadaadsda',
     db: 'mongodb://admin:ranmajung010@ds031359.mlab.com:31359/ecommerce_test',
     firebaseConfig : {
+
         databaseURL: 'https://oauth-242602.firebaseio.com',
         storageBucket: 'gs://oauth-242602.appspot.com',
         urlPath: 'https://firebasestorage.googleapis.com/v0/b/oauth-242602.appspot.com/o/'
